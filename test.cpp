a147be310fd5d9d6d970bb64461712ab92129f8a
darly17 <144932731+darly17@users.noreply.github.com>
2024-10-05 00:28:53 +0600

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,237 @@
+#include "pch.h"
+#include "C:\Users\User\Desktop\Vectors\Vectors.h"
+#include "C:\Users\User\Desktop\Vectors\Vectors_source.cpp"
+
+TEST(Test_deault_CONSTRUCTOR)
+{
+    Vectors a1;
+    EXPECT_TRUE((a1.get_Coordinates()[0] == 0) && (a1.get_Coordinates()[1] == 0)
+        && (a1.get_Coordinates()[2] == 0) && (a1.get_Coordinates()[3] == 0) &&
+        (a1.get_Coordinates()[4] == 0) && (a1.get_Coordinates()[5] == 0));
+}
+
+TEST(Test_CONSTRUCTOR_with_int_coordinates)
+{
+    Vectors a1(1, 2, 3, 4, 5, 6);
+    EXPECT_TRUE((a1.get_Coordinates()[0] == 1) && (a1.get_Coordinates()[1] == 2)
+        && (a1.get_Coordinates()[2] == 3) && (a1.get_Coordinates()[3] == 4) &&
+        (a1.get_Coordinates()[4] == 5) && (a1.get_Coordinates()[5] == 6));
+}
+
+TEST(Test_CONSTRUCTOR_with_int)
+{
+    Vectors a1(1, 2, 3, 4, 5, 6);
+    EXPECT_TRUE((a1.get_Coordinates()[0] == 1) && (a1.get_Coordinates()[1] == 2)
+        && (a1.get_Coordinates()[2] == 3) && (a1.get_Coordinates()[3] == 4) &&
+        (a1.get_Coordinates()[4] == 5) && (a1.get_Coordinates()[5] == 6));
+}
+TEST(Test_CONSTRUCTOR_with_double_and_negative_coordinates)
+{
+    Vectors a1(1, -2, 3, 4.98, -5, 6.09);
+    EXPECT_TRUE((a1.get_Coordinates()[0] == 1) && (a1.get_Coordinates()[1] == -2)
+        && (a1.get_Coordinates()[2] == 3) && (a1.get_Coordinates()[3] == 4.98) &&
+        (a1.get_Coordinates()[4] == -5) && (a1.get_Coordinates()[5] == 6.09));
+
+
+
+}
+
+
+
+TEST(Test_method_length_with_double_and_negative_coordinates)
+{
+    Vectors a1(1, -2, 3, 4.98, -5, 6.09);
+    EXPECT_EQ(a1.length_of_vector(), 5.864170870634655);
+}
+
+TEST(Test_method_length_with_int)
+{
+    Vectors a1(2, 2, 2, 2, 2, 4);
+    EXPECT_EQ(a1.length_of_vector(),2);
+
+}
+
+
+TEST(Test_method_summ_with_positiv)
+{
+    Vectors a1(2, 2, 2, 2, 2, 4);
+    Vectors b1(1, 2, 2, 3, 4, 4);
+    Vectors c1 = a1 + b1;
+    EXPECT_TRUE((c1.get_Coordinates()[0] == 3) && (c1.get_Coordinates()[1] == 4)
+        && (c1.get_Coordinates()[2] == 4) && (c1.get_Coordinates()[3] == 5)
+        && (c1.get_Coordinates()[4] == 6) && (c1.get_Coordinates()[5] == 8));
+}
+
+TEST(Test_method_summ_with_3)
+{
+    Vectors a1(2, 2, 2, 2, 2, 4);
+    Vectors b1(1, 2, 2, 3, 4, 4);
+    Vectors c1(1, 1, 1, 1, 1, 1);
+    Vectors d1;
+    d1 = a1 + b1 + c1;
+    EXPECT_TRUE((d1.get_Coordinates()[0] == 4) && (d1.get_Coordinates()[1] == 5)
+        && (d1.get_Coordinates()[2] == 5) && (d1.get_Coordinates()[3] == 6)
+        && (d1.get_Coordinates()[4] == 7) && (d1.get_Coordinates()[5] == 9));
+}
+
+TEST(Test_method_summ_ravn)
+{
+    Vectors a1(2, 2, 2, 2, 2, 4);
+    Vectors b1(1, 2, 2, 3, 4, 4);
+    a1 += b1;
+    EXPECT_TRUE((a1.get_Coordinates()[0] == 3) && (a1.get_Coordinates()[1] == 4)
+        && (a1.get_Coordinates()[2] == 4) && (a1.get_Coordinates()[3] == 5)
+        && (a1.get_Coordinates()[4] == 6) && (a1.get_Coordinates()[5] == 8));
+}
+
+TEST(Test_method_summ_ravn_with_3)
+{
+    Vectors a1(2, 2, 2, 2, 2, 4);
+    Vectors b1(1, 2, 2, 3, 4, 4);
+    Vectors c1(1, 1, 1, 1, 1, 1);
+    c1 += a1 + b1;
+    EXPECT_TRUE((c1.get_Coordinates()[0] == 4) && (c1.get_Coordinates()[1] == 5)
+        && (c1.get_Coordinates()[2] == 5) && (c1.get_Coordinates()[3] == 6)
+        && (c1.get_Coordinates()[4] == 7) && (c1.get_Coordinates()[5] == 9));
+}
+
+TEST(Test_method_razn)
+{
+  Vectors a1(2, 2, 2, 2, 2, 4);
+  Vectors b1(1, 2, 2, 3, 4, 4);
+  Vectors c1 = a1-b1;
+  EXPECT_TRUE((c1.get_Coordinates()[0] == 1) && (c1.get_Coordinates()[1] == 0)
+    && (c1.get_Coordinates()[2] == 0) && (c1.get_Coordinates()[3] == -1)
+    && (c1.get_Coordinates()[4] == -2) && (c1.get_Coordinates()[5] == 0));
+}
+
+TEST(Test_method_razn_with_3)
+{
+  Vectors a1(5, 8, 9, 0, 2, 4);
+  Vectors b1(1, 2, 2, 3, 4, 4);
+  Vectors c1(1, 1, 1, 1, 1, 1);
+  Vectors d1;
+  d1 = a1 - b1 - c1;
+  EXPECT_TRUE((d1.get_Coordinates()[0] == 3) && (d1.get_Coordinates()[1] == 5)
+    && (d1.get_Coordinates()[2] == 6) && (d1.get_Coordinates()[3] == -4)
+    && (d1.get_Coordinates()[4] == -3) && (d1.get_Coordinates()[5] == -1));
+}
+
+
+TEST(Test_method_razn_ravn)
+{
+    Vectors a1(2, 2, 2, 2, 2, 4);
+    Vectors b1(1, 2, 2, 3, 4, 4);
+    a1 -= b1;
+    EXPECT_TRUE((a1.get_Coordinates()[0] == 1) && (a1.get_Coordinates()[1] == 0)
+        && (a1.get_Coordinates()[2] == 0) && (a1.get_Coordinates()[3] == -1)
+        && (a1.get_Coordinates()[4] == -2) && (a1.get_Coordinates()[5] == 0));
+}
+
+TEST(Test_method_scalar_prod)
+{
+    Vectors a1(2, 2, 2, 2, 2, 4);
+    Vectors b1(1, 2, 2, 3, 4, 4);
+    double a;
+    a = a1 * b1;
+    EXPECT_EQ(a,4);
+}
+TEST(Test_method_scalar_prod_with_negative)
+{
+    Vectors a1(2, 2, 2, 2, 2, 4);
+    Vectors b1(1, 2, 8, 3, 4, 4);
+    double a;
+    a = a1 * b1;
+    EXPECT_EQ(a, -8);
+}
+TEST(Test_method_prod_with_negative_number)
+{
+    Vectors a1(2, 2, 2, 2, 2, 4);
+    Vectors d1;
+    double a=-8;
+    d1 = a1 * a;
+    
+    EXPECT_TRUE((d1.get_Coordinates()[0] == -16) && (d1.get_Coordinates()[1] == -16)
+        && (d1.get_Coordinates()[2] == -16) && (d1.get_Coordinates()[3] == -16)
+        && (d1.get_Coordinates()[4] == -16) && (d1.get_Coordinates()[5] == -32));
+}
+TEST(Test_method_prod_with_number)
+{
+    Vectors a1(2, 2, 2, 2, 2, 4);
+    double a=5;
+    a1 *= a;
+    EXPECT_TRUE((a1.get_Coordinates()[0] == 10) && (a1.get_Coordinates()[1] == 10)
+        && (a1.get_Coordinates()[2] == 10) && (a1.get_Coordinates()[3] == 10)
+        && (a1.get_Coordinates()[4] == 10) && (a1.get_Coordinates()[5] == 20));
+    
+}
+
+TEST(Test_method_div)
+{
+    Vectors a1(0, 0, 0, 4, 4, 4);
+    Vectors b1(0, 0, 0, 6, 6, 6);
+    double a,b;
+    b = 72.0 / 108;
+    a = a1/b1;
+    
+    EXPECT_TRUE(a==b);
+}
+
+TEST(Test_method_cos_between)
+{
+    Vectors a1(0, 0, 0, 4, 4, 4);
+    Vectors b1(0, 0, 0, 6, 6, 6);
+    double a;
+    
+    a = a1^b1;
+
+    EXPECT_TRUE(a == 1);
+}
+
+TEST(Test_method_more)
+{
+    Vectors a1(0, 0, 0, 4, 4, 4);
+    Vectors b1(0, 0, 0, 6, 6, 6);
+    
+
+    EXPECT_TRUE(b1>a1);
+}
+
+TEST(Test_method_less)
+{
+    Vectors a1(0, 0, 0, 4, 4, 4);
+    Vectors b1(0, 0, 0, 6, 6, 6);
+    
+
+    EXPECT_TRUE(a1<b1);
+}
+TEST(Test_method_less_or_eq)
+{
+    Vectors a1(0, 0, 0, 4, 4, 4);
+    Vectors b1(0, 0, 0, 6, 6, 6);
+    
+
+    EXPECT_TRUE(a1 <= b1);
+}
+TEST(Test_method_not_eq)
+{
+    Vectors a1(0, 0, 0, 4, 4, 4);
+    Vectors b1(0, 0, 0, 6, 6, 6);
+   
+
+    EXPECT_TRUE(a1 != b1);
+}
+TEST(Test_method_more_or_eq)
+{
+    Vectors a1(0, 0, 0, 4, 4, 4);
+    Vectors b1(0, 0, 0, 6, 6, 6);
+   
+    EXPECT_TRUE(b1>=a1);
+}
+TEST(Test_method_eq)
+{
+    Vectors a1(0, 0, 0, 4, 4, 4);
+    Vectors b1(0, 0, 0, 4, 4, 4);
+
+    EXPECT_TRUE(b1 == a1);
+}
